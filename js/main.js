283f54e2a9932492daf77efb92f80ea3d2f94216
shahobiddin <temirovshahobiddin@gmail.com>
2022-05-14 19:03:54 +0500

    adding new files

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper('#header__carousel', {
+    // Optional parameters
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.header__carousel-pagination',
+        clickable: true,
+    },
+});
+
+
+
